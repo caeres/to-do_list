1b51b8b82944f01dcd191279b2436d339375e10d
caeres <patrick.rubner97@gmail.com>
2021-03-22 19:19:07 +0100

    Finished project for now
    Mos tly works, some kind of error with the

--- class_todo.py
+++ class_todo.py
@@ -1,4 +1,4 @@
-#class in extra file - no idea whether thats best practice or not
+#class in extra file - seems to be best practice (prolly missing something tho)
 
 class ToDo:
     def __init__(self, due_to, task):
@@ -8,14 +8,18 @@ class ToDo:
         return f"To Do({self.due_to!r}, {self.task!r})"
     def __str__(self):
         return f"{self.due_to}: {self.task}"
+    
     #getter functions
     def get_task(self):
         return self.task
     def get_due_to(self):
         return self.due_to
-
-#OBSOLETE prolly
-    #prints due_date and task in "Date: Task" format (as a string)
-    #def print_info(self):
-    #   return f"{self.get_due_to()}: {self.get_task()}"
-
+    
+    #managing todos
+    def check_equal(self, other_todo):
+        if (self.due_to == other_todo.due_to 
+        and self.task == other_todo.task):
+            return True
+        else:
+            return False
+    
--- todo.py
+++ todo.py
@@ -1,48 +1,65 @@
-#script only to do list
-#current idea:
-# - every task is a string, tasks can be grouped in dictionaries with due dates or something
-# - all tasks saved in a dictionary
-# - file in and output as txt files? maybe csv?
-# - some simple commands are shown as the script is executed (print all tasks, print groups, add tasks, remove tasks, add groups, remove tasks)
-# - printing task overview when script is executed?
-# - due dates? how to add?
-# prolly creating and writing to a csv currently seems like the right call
-# for now using a txt tho, lets go
-
-#import csv
-
-# start off with very simple task list: no groups, no
-# created a class called to do, dont remember why (now i can just fill the dictionary with {group:list with tasks,...})
 import pickle
 from class_todo import ToDo
+import os
 
 
-with open("data/todo_list.txt", "rb") as tdl:
-    all_tasks = pickle.load(tdl)
+if "todo_list.txt" not in os.listdir("data"):
+    with open("data/todo_list.txt", "x") as tdl:
+        all_tasks = {}
+else:
+    with open("data/todo_list.txt", "r+") as tdl:
+        all_tasks = pickle.load(tdl)
 
 
-def create_task():
-    task = input("Which task should be added?")
+      
+def create_todo():
+    task = input("What task should be added?")
     due_date = input("When is the task due?")
     return ToDo(due_date, task)
 
-def create_group():
+#creating tasks and groups
+def create_group_in():
     groupname = input("Which group should be added?")
     all_tasks.update({groupname:[]})
+    print_list()
+
+def create_group(name):
+    all_tasks.update({name:[]})
+    print_list()
 
 def add_task_to_group():
     groupname = input("Which group do you want to add your task to?")
     if groupname not in all_tasks.keys():
+        query_nogroup = groupname
         query = input("""That group sadly doesn't exist. 
         Do you want to add your group as a new group to your to-do list?""")
         if query == "Yes":
-            create_group(query)
+            create_group(query_nogroup)
         else:
             print("Cancelling then.")
     else: 
-        all_tasks[groupname].append(create_task())
+        all_tasks[groupname].append(create_todo())
         print(f"New task was added to the group \'{groupname}\'!")
+    print_list()    
+#deleting tasks and groups
+def delete_group():
+    groupname = input("Which group should be deleted?")
+    if groupname in all_tasks:
+        del all_tasks[groupname]
+    else: 
+        print("This group is not included in your to-do list so far. Please try again!")
+    print_list()
+def delete_task():
+    task = input("Which task should be deleted?")
+    due = input("When is the task due?")
+    to_delete = ToDo(due, task)
+    for key in all_tasks.keys():
+        for item in all_tasks[key]:
+            if item.check_equal(to_delete):
+                all_tasks[key].remove(item)
+    print_list()
 
+#printing the list to console
 def print_list():
     print("YOUR TO-DO LIST:\n")
     if all_tasks != {}:
@@ -61,15 +78,35 @@ def print_list():
         print("""Seems like this is your first time using this script! Getting started is no problem,
         you can kick off your personal to-do list using the commands below. Just create a group, add some
         tasks to it and play around with the script!""")
-        print("The current functions only include creating tasks and task groups. There is no way to manage them yet.")
+    print("""You can currently use the commands: \"Print List\", \"Create Task\", \"Create Group\", \"Delete Task\"
+    and \"Delete Group\". To close this program, enter \"Exit\".""")
 
-#helper functions
-def create_group(name):
-    all_tasks.update({name:[]})
-
-all_tasks.clear()
 #print list at beginnnig
 print_list()
+#while loop to keep the programm running
+command = input("What do you want to do?")
+while command != 'Exit':
+    if command == "Create Task":
+        add_task_to_group()
+        command = input("What do you want to do?")
+    elif command == "Create Group":
+        create_group_in()
+        command = input("What do you want to do?")
+    elif command == "Print List":
+        print_list()
+        command = input("What do you want to do?")
+    elif command == "Delete Task":
+        delete_task()
+        command = input("What do you want to do?")
+    elif command == "Delete Group":
+        delete_group()
+        command = input("What do you want to do?")
+    else:
+        print("That command sadly doesn't exist.")
+        command = input("What do you want to do?")
+    if command=="Exit":
+        print("Goodbye!")
+    
 
 with open("data/todo_list.txt","wb") as tdl:
     pickle.dump(all_tasks, tdl)
